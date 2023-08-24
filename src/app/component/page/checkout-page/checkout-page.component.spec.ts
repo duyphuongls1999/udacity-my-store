d00d8939366cf94d142c4743323a4eae29e1d425
duyphuongls1999 <duyphuongls1999@gmail.com>
2023-08-24 22:23:39 +0700

    Fix UT checkout-page.component

--- src/app/component/page/checkout-page/checkout-page.component.spec.ts
+++ src/app/component/page/checkout-page/checkout-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CheckoutPageComponent } from './checkout-page.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CheckoutPageComponent', () => {
   let component: CheckoutPageComponent;
@@ -8,6 +9,9 @@ describe('CheckoutPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [CheckoutPageComponent]
     });
     fixture = TestBed.createComponent(CheckoutPageComponent);
